e290fcfd061d63b1f6d46d8e883db0165395fd8c
tyrollins <tylerrollins@outlook.com>
2016-03-13 08:54:37 -0700

    add footer

--- js/sandbox.js
+++ js/sandbox.js
@@ -740,6 +740,25 @@
     // // For Chrome, Safari, IE8+ and Opera 12+
     return message;
   };
+
+  $('.oi').click(function() {
+  var overlay = $('#overlay');
+  var featured = $('#featured');
+  if ($(this).hasClass('active')) {
+    $(this).removeClass('active');
+    overlay.fadeOut(300);
+    featured.animate({
+      'height': '0px'
+    }, 300);
+  } else {
+    $(this).addClass('active');
+    overlay.fadeIn(300);
+    featured.animate({
+      'height': '360px'
+    }, 300);
+  }
+});
+
 })();
 
 
